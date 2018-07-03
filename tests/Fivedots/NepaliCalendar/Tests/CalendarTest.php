d1e6f6b7239528709d5658aca742878bc14b3111
Sapin Bajracharya <sbnull@gmail.com>
2018-07-03 16:26:41 +0545

    add tests for earlier date range

--- tests/Fivedots/NepaliCalendar/Tests/CalendarTest.php
+++ tests/Fivedots/NepaliCalendar/Tests/CalendarTest.php
@@ -16,43 +16,117 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
      * @var Calendar
      */
     private $calendar;
-    /**
-     * @var array
-     */
-    protected $nepToEnglish = array(
-        'year' => 2014,
-        'month' => 12,
-        'date' => 31,
-        'day' => 'Wednesday',
-        'numDay' => 4,
-        'nmonth' => 'December'
-    );
-
-    /**
-     * @var array
-     */
-    protected $enToNepali = array(
-        'year' => 2071,
-        'month' => 9,
-        'date' => 16,
-        'day' => 'Wednesday',
-        'numDay' => 4,
-        'nmonth' => 'Poush'
-    );
 
     protected function setUp()
     {
         $this->calendar = new Calendar(new ArrayProvider());
     }
 
-
-    public function testNepaliToEnglishDate()
+    public function dateProvider()
     {
+        return [
+            [
+                [
+                    'year' => 2014,
+                    'month' => 12,
+                    'date' => 31,
+                    'day' => 'Wednesday',
+                    'numDay' => 4,
+                    'nmonth' => 'December'
+                ],
+                [
+                    'year' => 2071,
+                    'month' => 9,
+                    'date' => 16,
+                    'day' => 'Wednesday',
+                    'numDay' => 4,
+                    'nmonth' => 'Poush'
+                ],
+            ],
+            [
+                [
+                    'year' => 2018,
+                    'month' => 7,
+                    'date' => 3,
+                    'day' => 'Tuesday',
+                    'numDay' => 3,
+                    'nmonth' => 'July'
+                ],
+                [
+                    'year' => 2075,
+                    'month' => 3,
+                    'date' => 19,
+                    'day' => 'Tuesday',
+                    'numDay' => 3,
+                    'nmonth' => 'Ashar'
+                ],
+            ],
+            [
+                [
+                    'year' => 1942,
+                    'month' => 4,
+                    'date' => 13,
+                    'day' => 'Monday',
+                    'numDay' => 2,
+                    'nmonth' => 'April'
+                ],
+                [
+                    'year' => 1999,
+                    'month' => 1,
+                    'date' => 1,
+                    'day' => 'Monday',
+                    'numDay' => 2,
+                    'nmonth' => 'Baisakh'
+                ],
+            ],
+            [
+                [
+                    'year' => 1933,
+                    'month' => 4,
+                    'date' => 13,
+                    'day' => 'Thursday',
+                    'numDay' => 5,
+                    'nmonth' => 'April'
+                ],
+                [
+                    'year' => 1990,
+                    'month' => 1,
+                    'date' => 1,
+                    'day' => 'Thursday',
+                    'numDay' => 5,
+                    'nmonth' => 'Baisakh'
+                ],
+            ],
+            [
+                [
+                    'year' => 1943,
+                    'month' => 4,
+                    'date' => 13,
+                    'day' => 'Tuesday',
+                    'numDay' => 3,
+                    'nmonth' => 'April'
+                ],
+                [
+                    'year' => 1999,
+                    'month' => 12,
+                    'date' => 31,
+                    'day' => 'Tuesday',
+                    'numDay' => 3,
+                    'nmonth' => 'Chaitra'
+                ],
+            ],
+        ];
+    }
 
-
-        $date = $this->calendar->nepaliToEnglish(2071, 9, 16);
+    /**
+     *
+     * @dataProvider dateProvider
+     */
+    public function testNepaliToEnglishDate($ad, $bs)
+    {
+        $date = $this->calendar->nepaliToEnglish($bs['year'], $bs['month'], $bs['date']);
         $this->assertInternalType('array', $date);
-        $this->assertSame($this->nepToEnglish, $date);
+        $this->assertSame($ad, $date);    
     }
 
     /**
@@ -66,12 +140,13 @@ class CalendarTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers \Fivedots\NepaliCalendar\Calendar::englishToNepali()
      * @throws \Fivedots\NepaliCalendar\CalendarException
+     * @dataProvider dateProvider
      */
-    public function testEnglishToNepaliDate()
+    public function testEnglishToNepaliDate($ad, $bs)
     {
-        $date = $this->calendar->englishToNepali(2014, 12, 31);
+        $date = $this->calendar->englishToNepali($ad['year'], $ad['month'], $ad['date']);
         $this->assertInternalType('array', $date);
-        $this->assertSame($this->enToNepali, $date);
+        $this->assertSame($bs, $date);
     }
 
     /**
